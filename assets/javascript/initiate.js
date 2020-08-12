69b36d0eceea710085fec525c882723db25ebe1d
jcruz97 <cruzjurgen97@gmail.com>
2020-08-12 09:13:29 +0200

    merge dev => master

--- assets/javascript/initiate.js
+++ assets/javascript/initiate.js
@@ -1,6 +1,6 @@
 function letterButtons() {
     let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
-      `<button class="btn btn-lg btn-primary m-2" id='`+ letter +`' onClick="handleGuess('`+ letter +`')"> ` + letter + `</button>`).join('');
+      `<button class="btn btn-lg btn-dark m-2" id='`+ letter +`' onClick="handleGuess('`+ letter +`')"> ` + letter + `</button>`).join('');
   
     document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
